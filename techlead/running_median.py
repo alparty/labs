678584e55637939dedfa81acf483b9da099d70db
alex <23722276+alparty@users.noreply.github.com>
2019-10-08 15:40:52 +0200

    [Daily Problem] Running Median

---
+++ techlead/running_median.py
@@ -0,0 +1,16 @@
+"""
+You are given a stream of numbers. Compute the median for each new element .
+
+Eg. Given [2, 1, 4, 7, 2, 0, 5], the algorithm should output [2, 1.5, 2, 3.0, 2, 2, 2]
+"""
+
+def running_median(stream):
+  n = 0
+  a = 0.0
+  for i in stream:
+    a = (a*n + i)/(n+1)
+    n+=1
+    print a,
+
+running_median([2, 1, 4, 7, 2, 0, 5])
+# 2 1.5 2 3.0 2 2.0 2
